94888334da21a9d4d87c896cfa5f46fec7004d7b
JerryKaura <jerimkaura001@gmail.com>
2020-07-23 02:56:26 +0300

    added php class autoload function

--- index.php
+++ index.php
@@ -1,7 +1,29 @@
 <?php
 
-session_start();
 define('DS', DIRECTORY_SEPARATOR);
 define('ROOT', dirname(__FILE__)); 
+
+
+//load configufration and helper functions 
+require_once(ROOT . DS . 'config' .DS . 'config.php');
+require_once(ROOT . DS . 'app' . DS . 'lib'.DS. 'helpers' . DS . 'functions.php');
+
+//autoload our classes
+function autoload($classname){
+    if(file_exists(ROOT .DS .'core'. DS . $classname .'.php')){
+        require_once(ROOT .DS .'core'. DS . $classname .'.php');
+    }elseif(ROOT .DS .'app'. DS . 'controllers' .DS.  $classname .'.php'){
+        require_once(ROOT .DS .'app'. DS . 'controllers' . DS .  $classname .'.php');
+    }elseif(ROOT .DS .'app'. DS . 'models' .DS.  $classname .'.php'){
+        require_once(ROOT .DS .'app'. DS . 'models' .DS.  $classname .'.php');
+    }
+}
+
+spl_autoload_register('autoload');
+session_start();
+
+//split the url into an array of strings the remove whitespaces
 $url = isset($_SERVER['PATH_INFO'])? explode('/', ltrim($_SERVER['PATH_INFO'], '/')): [];
-require_once(ROOT . DS . 'core' . DS . 'bootstrap.php');
+
+//Route the requests
+Router::route($url);
